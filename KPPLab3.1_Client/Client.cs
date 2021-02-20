5742c30cdbb562cd59baf0333bb18d4b8b022f3d
Nyt1k <61513369+Nyt1k@users.noreply.github.com>
2021-02-21 00:25:51 +0200

    Add project files.

---
+++ KPPLab3.1_Client/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace KPPLab3._1_Client
+{
+    public class Client
+    {
+      
+        public static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = new TcpClient("127.0.0.1", 8080);
+
+                    
+                    Console.Write("Enter line: ");
+                    var message = Console.ReadLine();
+
+                    int byteCount = Encoding.ASCII.GetByteCount(message + 1);
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(data, 0, data.Length);
+
+                    StreamReader reader = new StreamReader(stream);
+                    string answer = reader.ReadLine();
+                    Console.WriteLine(answer);
+
+                    stream.Close();
+                    client.Close();
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
+
+
---
+++ KPPLab3.1_Server/Server.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace KPPLab3._1
+{
+    public class Server
+    {
+        public static string ChangeString(string data)
+        {
+            string ndata = "";
+
+            int n = data.Length / 2;
+            if (data.Length % 2 == 0)
+            {
+
+                ndata = data.Remove(n - 1, 2);
+
+            }
+            else
+            {
+                ndata = data.Remove(n, 1);
+            }
+
+
+
+            return ndata;
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+                listener.Start();
+
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " - Server is ready to work");
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " - Waiting...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    StreamWriter writer = new StreamWriter(client.GetStream());
+
+                    byte[] buffer = new byte[256];
+                    stream.Read(buffer, 0, buffer.Length);
+                    int recv = 0;
+                    foreach(byte b in buffer)
+                    {
+                        if(b!=0)
+                        {
+                            recv++;
+                        }
+                    }
+                    string str = Encoding.UTF8.GetString(buffer,0,recv);
+                    
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() +  " - Received data: " + str);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + " - Working...");
+
+                    string nstr = ChangeString(str);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + " - Result: " + nstr);
+                    buffer = Encoding.UTF8.GetBytes(nstr);
+
+                    writer.WriteLine(nstr);
+                    writer.Flush();
+                }
+
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+
+            System.Threading.Thread.Sleep(10000);
+        }
+    }
+}
